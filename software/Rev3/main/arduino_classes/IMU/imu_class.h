63e4c506e8b7ab9e3a3f0f77290a9f1bf87ce036
lilibaker <65792993+lilibaker@users.noreply.github.com>
2023-03-29 18:32:56 -0400

    Add global accel variables

--- software/Rev3/main/arduino_classes/IMU/imu_class.h
+++ software/Rev3/main/arduino_classes/IMU/imu_class.h
@@ -8,16 +8,20 @@ class Imu {
   private:
     byte pin;
     Adafruit_BNO055 bno;
+    float x_accel_global;
+    float y_accel_global;
+    float z_accel_global;
     
   public:
     Imu();
     void init();
     void test_connection();
-    void read_quaternions();
+    imu::Quaternion read_quaternions();
     void read_euler();
     void read_gravity();
     void read_gyroscope();
-    void read_accelerometer();
+    imu::Vector<3> read_accelerometer();
     void read_linear_accel();
+    void rotate();
     void print_data(imu::Vector<3> data);
 };
