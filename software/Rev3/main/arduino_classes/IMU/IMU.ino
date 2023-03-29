9434ef06b7472dd4065e2f6469b5669ec5234d80
lilibaker <65792993+lilibaker@users.noreply.github.com>
2023-03-29 17:24:45 -0400

    Fix IMUf file structure

---
+++ software/Rev3/main/arduino_classes/IMU/IMU.ino
@@ -0,0 +1,18 @@
+#include "imu_class.h"
+
+//setup for testing 
+byte IICdata[5] = {0,0,0,0,0}; //buffer for sensor data
+
+// declare instance of Imu as global for scope
+Imu_oop test;
+
+
+void setup() {
+//  Wire.begin();
+//  Serial.begin(9600);
+}
+
+void loop() {
+//  test.test_connection();
+  test.read_euler(); 
+}
