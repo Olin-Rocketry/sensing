c6a99479ef0151280ec56795942f4e7a4ec4aae0
lilibaker <65792993+lilibaker@users.noreply.github.com>
2023-04-04 21:09:11 -0400

    Add testing code

--- software/Rev3/PRISM_main/PRISM_main.ino
+++ software/Rev3/PRISM_main/PRISM_main.ino
@@ -1,14 +1,27 @@
 #include "arduino_classes/LED/led_class.h"
+#include "arduino_classes/Data/data_class.h"
+#include "arduino_classes/IMU/imu_class.h"
 
 
 
+//setup for testing 
+byte IICdata[5] = {0,0,0,0,0}; //buffer for sensor data
+
+Led led1(12);
+// declare instance of Imu as global for scope
+Data data;
+
+Imu test(data);
 
 void setup() {
-  
-
+//  Wire.begin();
+//  Serial.begin(9600);
+test.begin_imu();
 }
 
 void loop() {
-  
-
+//  test.test_connection();
+//  test.read_accelerometer(); 
+test.rotate();
+Serial.println(data.accelx());
 }
