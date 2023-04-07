adb35c6b5fdd19941d0595f94b3888d8bca49421
lilibaker <65792993+lilibaker@users.noreply.github.com>
2023-04-07 19:16:05 -0400

    Fix integration of imu and data in main

--- software/Rev3/PRISM_main/PRISM_main.ino
+++ software/Rev3/PRISM_main/PRISM_main.ino
@@ -1,36 +1,40 @@
-#include "arduino_classes/LED/led_class.h"
-#include "arduino_classes/Data/data_class.h"
-#include "arduino_classes/IMU/imu_class.h"
+//#include "led_class.h"
+#include "data_class.h"
+#include "imu_class.h"
+#include <Arduino.h>
+
 
 // setup for testing
 byte IICdata[5] = {0, 0, 0, 0, 0}; // buffer for sensor data
 
-Led led1(12);
+//Led led1(12);
 // declare instance of Imu as global for scope
 Data data;
 
-Imu test(data);
-
+Imu test(&data);
 void setup()
 {
-  //  Wire.begin();
-  //  Serial.begin(9600);
-  test.begin_imu();
+    //  Wire.begin();
+      Serial.begin(9600);
+      data.SDbegin();
+
+      test.begin_imu();
+      test.test_connection();
 }
 
 void loop()
 {
-  //  test.test_connection();
-  //  test.read_accelerometer();
-  test.rotate();
-  Serial.println(data.accelx());
+      
+    //  test.read_accelerometer();
+    test.rotate();
+    Serial.println(data.accelx());
 }
-void loop()
-{
-  //    led1.red();
-  //    delay(1000);
-  //    led1.green();
-  //    delay(1000);
-  //    led1.off();
-  //    delay(1000);
-}
+//void loop()
+//{
+//      led1.red();
+//      delay(1000);
+//      led1.green();
+//      delay(1000);
+//      led1.off();
+//      delay(1000);
+//}
