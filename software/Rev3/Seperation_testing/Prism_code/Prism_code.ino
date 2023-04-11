8d51127bf05bf71e946f62c899880eda4236698b
Cbabe <78317842+Cbabe@users.noreply.github.com>
2023-04-10 20:12:05 -0400

    Seperation

---
+++ software/Rev3/Seperation_testing/Prism_code/Prism_code.ino
@@ -0,0 +1,29 @@
+#define Main 1 //MUST change
+#define Drog 2 //MUST change
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Drog,OUTPUT);
+  pinMode(Main,OUTPUT);
+  digitalWrite(Drog, LOW);
+  digitalWrite(Main, LOW);
+Serial.begin(9600);
+Serial1.begin(9600); //MUST CHANGE
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (Serial1.available()>0){
+  if (Serial1.read()=='d'){
+    digitalWrite(Drog, HIGH);
+    delay(1000);
+    digitalWrite(Drog, LOW);
+    Serial.println("BOOM DROG");
+  }
+  if (Serial1.read()=='d'){
+    digitalWrite(Drog, HIGH);
+    delay(1000);
+    digitalWrite(Drog, LOW);
+    Serial.println("BOOM DROG");
+  }
+}
+}
