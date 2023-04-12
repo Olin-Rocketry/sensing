9ebfc39468aa623794353d0521ee6f6d6acb1831
lilibaker <65792993+lilibaker@users.noreply.github.com>
2023-04-12 17:20:20 -0400

    Remove unnecessary comment

--- software/Rev3/PRISM_main/altimeter_class.h
+++ software/Rev3/PRISM_main/altimeter_class.h
@@ -7,18 +7,18 @@
 #include "Adafruit_BMP3XX.h"
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+class Altimeter
+{
 
-class Altimeter {
-  
-  private:
-    Data *data;
-    Adafruit_BMP3XX bmp;
-    
-  public:
-    Altimeter(Data *data);// not used
-    void init();
-    void begin_altimeter();
-    void read_altitude();
-    void read_pressure();
-    void read_temperature();
+private:
+  Data *data;
+  Adafruit_BMP3XX bmp;
+
+public:
+  Altimeter(Data *data);
+  void init();
+  void begin_altimeter();
+  void read_altitude();
+  void read_pressure();
+  void read_temperature();
 };
