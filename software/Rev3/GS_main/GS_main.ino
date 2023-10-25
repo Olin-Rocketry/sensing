fa0b85d792f83f591026010aa725e49f29c217ba
Rowan Jansens <rowan.jansens@gmail.com>
2023-10-25 18:12:53 -0400

    Update GS_main.ino

--- software/Rev3/GS_main/GS_main.ino
+++ software/Rev3/GS_main/GS_main.ino
@@ -1,6 +1,11 @@
 #include "radio_class.h"
 
 Radio radio;
+<<<<<<< Updated upstream
+=======
+bool connected;
+void connect_GS();
+>>>>>>> Stashed changes
 
 void setup()
 {
@@ -8,13 +13,40 @@ void setup()
   Serial.begin(115200);
   radio.begin();
   while(!Serial){};
+<<<<<<< Updated upstream
   delay(100);
   Serial.write(64);
+=======
+  connect_GS();
+  
+>>>>>>> Stashed changes
   
 //  while (!Serial.available());
 }
 
 void loop()
 {
+<<<<<<< Updated upstream
   radio.receivedPacket();
+=======
+  if (connected){
+    radio.receivedPacket();
+  }
+  
+  else{
+    connect_GS();
+}
+
+  while(!Serial){
+    connected = false;
+  }
+  
+  
+}
+
+
+void connect_GS(){
+  Serial.write(64);
+  connected = true;
+>>>>>>> Stashed changes
 }
