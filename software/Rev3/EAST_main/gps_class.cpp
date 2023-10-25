f6f3e057753763e95a91da7e4e76b4082310a3e9
lilibaker <65792993+lilibaker@users.noreply.github.com>
2023-10-25 18:33:10 -0400

    Fix error issues

--- software/Rev3/EAST_main/gps_class.cpp
+++ software/Rev3/EAST_main/gps_class.cpp
@@ -53,6 +53,9 @@ void Gps::read_position()
         gpsStruct.diagmsg = pow(2,2);
         statusLed->RGB(1, 255, 0, 255);
     }
+    radio->EAST_serial.sendDatum(gpsStruct);
+
+    Serial8.flush();
 }
 
 void Gps::read_data()
@@ -88,9 +91,9 @@ void Gps::read_data()
     else{
         gpsStruct.diagmsg = pow(2, 2);
     }
-    radio->EAST_serial.sendDatum(gpsStruct);
-
-    Serial8.flush();
+//    radio->EAST_serial.sendDatum(gpsStruct);
+//
+//    Serial8.flush();
 
 //    Serial.println(micros() - t_1);
 
