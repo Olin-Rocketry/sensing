f62f65b9e8d45297601c41ea9341835b907ac85c
ishanporwal <ishanporwaltennis@gmail.com>
2024-02-21 17:37:20 -0500

    Updated Rev4 based on new changes to components
    Combined EAST and Prism. Updated IMU class with appropriate IMU. Added code for buzzer.

--- software/Rev4/PRISM_main/imu_class.h
+++ software/Rev4/PRISM_main/imu_class.h
@@ -1,43 +1,42 @@
 #pragma once
 #include "data_class.h"
 #include <Arduino.h>
-#include <Adafruit_BNO055.h>
+#include <Wire.h>
+#include <Adafruit_LSM9DS1.h>
+#include <Adafruit_Sensor.h> // not used in this demo but required!
 
 class Imu
 {
-
 private:
-  Data *data;
-  Adafruit_BNO055 bno;
-  
-  double dt;
-  double old_time=0;
-  double new_time;
-  
-  bool debugEnable; //Enable debug printing
+    Data *data;
+    Adafruit_LSM9DS1 lsm;
 
+    double dt;
+    double old_time = 0;
+    double new_time;
+
+    bool debugEnable; // Enable debug printing
 
 public:
-  Imu(Data *data);
-  void init();
-  void begin_imu(bool debugEnable);
-  void test_connection();
-  imu::Quaternion read_quaternions();
-
-
-  
-  void perform_reading(); // call rotate() and read_gyroscope()
-  /* Rotate the acceleration into the global refernce frame.
+    Imu(Data *data);
+    void init();
+    void setupSensor();
+    void begin_imu(bool debugEnable);
+    void test_connection();
+    imu::Quaternion read_quaternions();
+    /* Rotate the acceleration into the global reference frame.
    *
    * Get the linear acceleration. Get the quaternion of the chip.
    * Rotate the acceleration by the defined quaternion for the rocket
    * orientation.
-   */
-  void rotate();
-  imu::Vector<3> read_euler(); // from the chip
-  void read_gravity();
-  void read_gyroscope();
-  imu::Vector<3> read_accelerometer();
-  imu::Vector<3> read_linear_accel();
-  void print_data(imu::Vector<3> data);
+   */    void perform_reading(); // call rotate() and read_gyroscope()
+    void rotate();
+    imu::Vector<3> read_euler();        // from the chip
+    imu::Vector<3> read_gravity();
+    imu::Vector<3> read_gyroscope();
+    imu::Vector<3> read_accelerometer();
+    imu::Vector<3> read_linear_accel();
+    void print_data(imu::Vector<3> data);
+    imu::Quaternion ToQuaternion(double roll, double pitch, double yaw); // Quaternion calculation
+    void set(Quaternion q1); // Set Euler angles from Quaternion
 };
---
+++ software/Rev4/PRISM_main/radio_class.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "led_class.h"
+#include <Arduino.h>
+#include <algorithm>
+#include <RH_RF95.h>
+#include <SerialTransfer.h>
+
+
+#define RFM95_CS 0
+#define RFM95_RST 20
+#define RFM95_INT 21
+#define RF95_FREQ 915.0
+
+class Radio {
+  private:
+    RH_RF95 rf95;
+    const static int packetSize = 28;
+    float packet[packetSize];
+    char encodedPacket[packetSize*4];
+    union floatunion_t;
+    float decoder(char* encoded);
+    char serialBuffer[packetSize*4];
+    Led *statusLed;
+    bool serial_status;
+
+
+    
+    
+
+  public:
+  
+//    Radio(Led *statusLed);
+    Radio() : rf95(RFM95_CS,RFM95_INT) { }  //what is going on here? We need to make this a constructor method 
+
+    SerialTransfer EAST_serial;
+
+    
+    void init();  
+    void begin(); 
+    void reveicePacket();  
+    void led_test(Led *statusLed);
+ 
+    void sendRadio(char serialBuffer[packetSize*4]);
+//void sendRadio();
+void readRadio();
+    void decodeData();  
+    void printData();
+    void sendingPacket();
+    char readSerial();
+
+};
