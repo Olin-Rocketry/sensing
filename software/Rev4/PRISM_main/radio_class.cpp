e67d2ce74581d8ab7db504a3f84941cd3d12baa0
Daniel Theunissen <theundaniel@gmail.com>
2025-03-13 16:40:16 -0400

    Add support for RFM69 Module

--- software/Rev4/PRISM_main/radio_class.cpp
+++ software/Rev4/PRISM_main/radio_class.cpp
@@ -1,6 +1,8 @@
 #include "radio_class.h"
 #include <algorithm>
 
+#define RFM69 // Comment for RF95 Module
+
 void Radio::init(){
     //init both un-encoded and encoded packet with 0
     std::fill_n(packet, packetSize, 0.0000);
@@ -15,51 +17,86 @@ void Radio::led_test(Led *statusLed){
 }
 
 void Radio::begin(){
+    #ifdef RFM69
+      pinMode(RFM69_RST, OUTPUT);
+      digitalWrite(RFM69_RST, LOW);
 
-    //radio stuff
-    pinMode(RFM95_RST, OUTPUT);
-    digitalWrite(RFM95_RST, HIGH);
-    Serial.println("Arduino LoRa RX Test!");
-    // manual reset
-    digitalWrite(RFM95_RST, LOW);
-    delay(10);
-    digitalWrite(RFM95_RST, HIGH);
-    delay(10);
+      Serial.println("Feather RFM69 TX Test!");
+      Serial.println();
 
-    while (!rf95.init())
-    {
-        Serial.println("LoRa radio init failed");
+      // manual reset
+      digitalWrite(RFM69_RST, HIGH);
+      delay(10);
+      digitalWrite(RFM69_RST, LOW);
+      delay(10);
+
+      if (!rf69.init()) {
+        Serial.println("RFM69 radio init failed");
         statusLed->RGB2(0, 255, 0, 0);
-        while (1)
-            ;
-    }
-    Serial.println("LoRa radio init OK!");
-    statusLed->RGB2(0, 0, 0, 255);
-
-    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
-    if (!rf95.setFrequency(RF95_FREQ))
-    {
+        while (1);
+      }
+      Serial.println("RFM69 radio init OK!");
+      // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
+      // No encryption
+      if (!rf69.setFrequency(RF69_FREQ)) {
         Serial.println("setFrequency failed");
-        while (1)
-            ;
-    }
-    Serial.print("Set Freq to: ");
-    Serial.println(RF95_FREQ);
+        statusLed->RGB2(0, 0, 0, 255);
+      }
 
-    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-    // attempt to speed up radio with Bw = 500 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Fast+short range
-    rf95.setModemConfig(1);
-    // The default transmitter power is 13dBm, using PA_BOOST.
-    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-    // you can set transmitter powers from 5 to 23 dBm:
-    rf95.setTxPower(13, false);
+      // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
+      // ishighpowermodule flag set like this:
+      rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+
+      // // The encryption key has to be the same as the one in the server
+      // uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+      //                   0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+      // rf69.setEncryptionKey(key);
+
+      Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
+    #else
+      //radio stuff
+      pinMode(RFM95_RST, OUTPUT);
+      digitalWrite(RFM95_RST, HIGH);
+      Serial.println("Arduino LoRa RX Test!");
+      // manual reset
+      digitalWrite(RFM95_RST, LOW);
+      delay(10);
+      digitalWrite(RFM95_RST, HIGH);
+      delay(10);
+
+      while (!rf95.init())
+      {
+          Serial.println("LoRa radio init failed");
+          statusLed->RGB2(0, 255, 0, 0);
+          while (1);
+      }
+      Serial.println("LoRa radio init OK!");
+      statusLed->RGB2(0, 0, 0, 255);
+
+      // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+      if (!rf95.setFrequency(RF95_FREQ))
+      {
+          Serial.println("setFrequency failed");
+          while (1);
+      }
+      Serial.print("Set Freq to: ");
+      Serial.println(RF95_FREQ);
+
+      // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+      // attempt to speed up radio with Bw = 500 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Fast+short range
+      rf95.setModemConfig(1);
+      // The default transmitter power is 13dBm, using PA_BOOST.
+      // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+      // you can set transmitter powers from 5 to 23 dBm:
+      rf95.setTxPower(13, false);
 
 
 
-     delay(10);
-    //initalize EAST serial communication
-    // Serial8.begin(800000);  //east uses 8, prism uses 5
-    // EAST_serial.begin(Serial8);
+      delay(10);
+      //initalize EAST serial communication
+      // Serial8.begin(800000);  //east uses 8, prism uses 5
+      // EAST_serial.begin(Serial8);
+    #endif
 }
 
 union Radio::floatunion_t
@@ -114,29 +151,46 @@ void Radio::sendingPacket()
  }
 
 void Radio::reveicePacket()
-{
-    if (rf95.available())
-    {
+{   
+    #ifdef RFM69
+      if (rf69.available())
+      {
         readRadio();
         decodeData();
         printData();
-        
-    }
+      }
+    #else
+      if (rf95.available())
+      {
+        readRadio();
+        decodeData();
+        printData();
+      }
+    #endif
 }
 
 void Radio::sendRadio(){
   
 //    Serial.println(serialBuffer);
     // Send a message to rf95_server
-  if(data->validpacket)
-  {
-    rf95.waitPacketSent();
-    rf95.send((uint8_t *)data->encodedpacket, sizeof(data->encodedpacket));
-    data->validpacket=false;
-  }
-//    delay(10);
-//    rf95.waitPacketSent();
-//    Serial.println("Packet Sent");
+  #ifdef RFM69
+    if(data->validpacket)
+    {
+      rf69.waitPacketSent();
+      rf69.send((uint8_t *)data->encodedpacket, sizeof(data->encodedpacket));
+      data->validpacket=false;
+    }
+  #else
+    if(data->validpacket)
+    {
+      rf95.waitPacketSent();
+      rf95.send((uint8_t *)data->encodedpacket, sizeof(data->encodedpacket));
+      data->validpacket=false;
+    }
+  //    delay(10);
+  //    rf95.waitPacketSent();
+  //    Serial.println("Packet Sent");
+  #endif
 }
 
 void Radio::readRadio()
@@ -144,24 +198,43 @@ void Radio::readRadio()
     // Should be a message for us now
     uint8_t serialBuffer[200]; // RH_RF95_MAX_MESSAGE_LEN
     uint8_t len = sizeof(serialBuffer);
-
-    if (rf95.recv(serialBuffer, &len))
-    {
-        // RH_RF95::printserialBufferfer("Received: ", serialBuffer, len);
-        Serial.print("Got: ");
-        Serial.println((char *)serialBuffer);
-        Serial.print("RSSI: ");
-        Serial.println(rf95.lastRssi(), DEC);
-    }
-    else
-    {
-        Serial.println("Receive failed");
-    }
-    for (int i = 0; i < packetSize * 4; i++)
-    {
-        //      Serial.println((char)serialBuffer[i]);
-        encodedPacket[i] = (char)serialBuffer[i];
-    }
+    #ifdef RFM69
+      if (rf69.recv(serialBuffer, &len))
+      {
+          // RH_RF95::printserialBufferfer("Received: ", serialBuffer, len);
+          Serial.print("Got: ");
+          Serial.println((char *)serialBuffer);
+          Serial.print("RSSI: ");
+          Serial.println(rf69.lastRssi(), DEC);
+      }
+      else
+      {
+          Serial.println("Receive failed");
+      }
+      for (int i = 0; i < packetSize * 4; i++)
+      {
+          //      Serial.println((char)serialBuffer[i]);
+          encodedPacket[i] = (char)serialBuffer[i];
+      }
+    #else
+      if (rf95.recv(serialBuffer, &len))
+      {
+          // RH_RF95::printserialBufferfer("Received: ", serialBuffer, len);
+          Serial.print("Got: ");
+          Serial.println((char *)serialBuffer);
+          Serial.print("RSSI: ");
+          Serial.println(rf95.lastRssi(), DEC);
+      }
+      else
+      {
+          Serial.println("Receive failed");
+      }
+      for (int i = 0; i < packetSize * 4; i++)
+      {
+          //      Serial.println((char)serialBuffer[i]);
+          encodedPacket[i] = (char)serialBuffer[i];
+      }
+    #endif
 }
 
 void Radio::decodeData()
--- software/Rev4/PRISM_main/radio_class.h
+++ software/Rev4/PRISM_main/radio_class.h
@@ -3,16 +3,30 @@
 #include <Arduino.h>
 #include <algorithm>
 #include <RH_RF95.h>
+#include <RH_RF69.h>
 #include "data_class.h"
 
-#define RFM95_CS 9
-#define RFM95_RST 8
-#define RFM95_INT 7
-#define RF95_FREQ 915.0
+#define RFM69 // Comment for RF95 Module
+
+#ifdef RFM69
+  #define RFM69_CS 9
+  #define RFM69_RST 8
+  #define RFM69_INT 7
+  #define RF69_FREQ 434.0
+#else
+  #define RFM95_CS 9
+  #define RFM95_RST 8
+  #define RFM95_INT 7
+  #define RF95_FREQ 915.0
+#endif
 
 class Radio {
   private:
-    RH_RF95 rf95;
+    #ifdef RFM69
+      RH_RF69 rf69;
+    #else
+      RH_RF95 rf95;
+    #endif
     const static int packetSize = 28;
     float packet[packetSize];
     char encodedPacket[packetSize*4];
@@ -29,7 +43,11 @@ class Radio {
   public:
 
 //    Radio(Led *statusLed);
-    Radio(Data *data) : rf95(RFM95_CS,RFM95_INT) {this->data = data; init();} ;
+    #ifdef RFM69
+      Radio(Data *data) : rf69(RFM69_CS, RFM69_INT) {this->data = data; init();} ;
+    #else
+      Radio(Data *data) : rf95(RFM95_CS,RFM95_INT) {this->data = data; init();} ;
+    #endif
 
     
     void init();  
